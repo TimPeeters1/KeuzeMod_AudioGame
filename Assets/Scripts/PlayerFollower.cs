fe506a523bbefa1e5d12987e3c11d9695085484a
Tim Peeters <tim.peeters12345@gmail.com>
2019-10-01 16:34:27 +0200

    Project Init

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    [SerializeField] float walkSpeed;
+
+    [SerializeField] CinemachineVirtualCamera vCamObject;
+
+    [SerializeField] bool doWalk;
+
+    private void Start()
+    {
+        vCamObject = GetComponent<CinemachineVirtualCamera>();
+
+        vCamObject.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition = 0f;
+    }
+
+    private void Update()
+    { 
+        if (doWalk)
+        {
+            doMovement();
+        }
+    }
+
+    void doMovement()
+    {
+        vCamObject.GetCinemachineComponent<CinemachineTrackedDolly>().m_PathPosition += walkSpeed * 0.001f;
+    }
+}
